ce9f99d7eedd2b9fb3a57783aa69ad0e22868444
adonegan <48014273+adonegan@users.noreply.github.com>
2020-12-30 00:57:14 +0000

    add Nicolas Cage ref for context

--- cage_web_scrape/best_of_cage.py
+++ cage_web_scrape/best_of_cage.py
@@ -1,14 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-# scrape this URL
+# scrape this URL below
+# url contains list of Nicolas Cage's films and misc info
 url = 'https://www.imdb.com/list/ls003862365/?sort=list_order,asc&st_dt=&mode=simple&page=1&ref_=ttls_vw_smp'
 response = requests.get(url)
-# get it in lxml format
+# get url/page in lxml format
 soup = BeautifulSoup(response.text, 'lxml')
-# find everything this div element
+# find everything in this div element
 films = soup.find_all('div', 'col-title')
 for film in films:
     # loop over each film and find the following:
+    # name of film, year it was made, list number
     nameOfFilm= film.find('a').contents[0]
     yearOfFilm = film.find('span','lister-item-year').text
     indexOfFilm = film.find('span', 'lister-item-index').text
