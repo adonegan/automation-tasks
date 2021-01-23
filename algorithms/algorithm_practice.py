0658733313533e051c0a076adb7bf400f8613e12
adonegan <48014273+adonegan@users.noreply.github.com>
2021-01-23 15:11:09 +0000

    first program with notes

---
+++ algorithms/algorithm_practice.py
@@ -0,0 +1,26 @@
+# A file to practice algorithms
+# Write algorithm on paper first
+# Then convert to code
+
+# PROGRAM 1
+# Ask the user for two numbers and add them
+
+# inital values
+x = 0
+y = 0
+sum = 0
+
+# ask user for input
+# store input in variables
+x = input('Pick a number: ')
+y = input('Pick another number: ')
+
+# convert to ints in new variables
+# add new variables together
+newX = int(x)
+newY = int(y)
+sum = newX + newY
+
+# print output of results
+print('The sum is... ')
+print(sum)
